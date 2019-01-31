a4c2986cc12b30ad0ac7dcda77a3cb798e6da129
Tony Bowden <tony@tmtm.com>
2019-01-31 12:57:32 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,36 +1,33 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
 require 'nokogiri'
-require 'colorize'
-require 'pry'
+require 'scraped'
+require 'scraperwiki'
+
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
-
-class String
-  def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
-  end
-end
 
 def noko_for(url)
   Nokogiri::HTML(open(url).read)
 end
 
-def scrape_list(url)
+def term_data(url)
   noko = noko_for(url)
-  noko.xpath('//h3[span[@id="Mandature_mars_2012_-_2017"]]/following-sibling::table[1]//tr[td]').each do |tr|
+  noko.xpath('//h3[span[@id="Mandature_mars_2012_-_2017"]]/following-sibling::table[1]//tr[td]').map do |tr|
     tds = tr.css('td')
-    data = { 
-      name: tds[0].text,
+    {
+      name: tds[0].text.tidy,
       wikiname: tds[0].xpath('.//a[not(@class="new")]/@title').text,
-      party: tds[1].text,
+      party: tds[1].text.tidy,
       term: 2012,
       source: url,
     }
-    ScraperWiki.save_sqlite([:name, :party, :term], data)
   end
 end
 
-scrape_list('https://fr.wikipedia.org/wiki/Conseil_territorial_de_Saint-Barth%C3%A9lemy#Mandature_mars_2012_-_2017')
+data = term_data('https://fr.wikipedia.org/wiki/Conseil_territorial_de_Saint-Barth%C3%A9lemy')
+data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
+
+ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
+ScraperWiki.save_sqlite([:name, :party, :term], data)
