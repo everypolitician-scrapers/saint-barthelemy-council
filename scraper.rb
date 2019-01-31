a8e7b281c18063d0ec3bc9d85798f5f7763b874f
Tony Bowden <tony@tmtm.com>
2019-01-31 13:01:14 +0000

    Get data for all terms

--- scraper.rb
+++ scraper.rb
@@ -12,21 +12,21 @@ def noko_for(url)
   Nokogiri::HTML(open(url).read)
 end
 
-def term_data(url)
+def member_data(url)
   noko = noko_for(url)
-  noko.xpath('//h3[span[@id="Mandature_mars_2012_-_2017"]]/following-sibling::table[1]//tr[td]').map do |tr|
+  noko.xpath('//table[.//th[contains(.,"Fonction")]]//tr[td]').map do |tr|
     tds = tr.css('td')
     {
       name: tds[0].text.tidy,
       wikiname: tds[0].xpath('.//a[not(@class="new")]/@title').text,
       party: tds[1].text.tidy,
-      term: 2012,
+      term: tr.xpath('.//preceding::h3').last.text[/(\d{4})/, 1],
       source: url,
     }
   end
 end
 
-data = term_data('https://fr.wikipedia.org/wiki/Conseil_territorial_de_Saint-Barth%C3%A9lemy')
+data = member_data('https://fr.wikipedia.org/wiki/Conseil_territorial_de_Saint-Barth%C3%A9lemy')
 data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
 
 ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
